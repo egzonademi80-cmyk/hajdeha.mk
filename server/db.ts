72c1e5fb6311a7ccb70d291078fe93828f9f662b
egzonademi80-cmyk <egzonademi80@gmail.com>
2026-01-31 04:06:37 +0000

    Fix db.ts: use direct Postgres connection string for serverless, remove .js from schema import

--- server/db.ts
+++ server/db.ts
@@ -1,16 +1,17 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "../shared/schema.js";
+import * as schema from "../shared/schema"; // <-- pa .js
 
 const { Pool } = pg;
 
-// We use the environment variable which the user has set in the Replit secrets
-const connectionString = 'postgresql://neondb_owner:npg_eZ0IwR3cXxuD@ep-tiny-rain-ah5sdn17-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require';
+// Direkt connection string (përdor vetë stringun tënd)
+const connectionString =
+  "postgresql://neondb_owner:npg_eZ0IwR3cXxuD@ep-tiny-rain-ah5sdn17-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require";
 
 export const pool = new Pool({
   connectionString,
-  ssl: { rejectUnauthorized: false },
-  max: 1, 
+  ssl: { rejectUnauthorized: false }, // Serverless friendly SSL
+  max: 1, // Limit connection per serverless function
 });
 
 export const db = drizzle(pool, { schema });
