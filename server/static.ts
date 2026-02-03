7c60737903479052534868890d6f1d1be83dd259
egzonademi80-cmyk <egzonademi80@gmail.com>
2026-02-03 03:16:31 +0000

    Fix sitemap.xml serving with correct MIME type

--- server/static.ts
+++ server/static.ts
@@ -1,15 +1,34 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
-
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
-
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
     );
   }
+  // Serve sitemap with correct MIME type
+  app.get("/sitemap.xml", (_req, res) => {
+    res.setHeader("Content-Type", "application/xml");
+    const sitemapPath = path.join(distPath, "sitemap.xml");
+    if (fs.existsSync(sitemapPath)) {
+      res.sendFile(sitemapPath);
+    } else {
+      res.status(404).send("Sitemap not found");
+    }
+  });
+
+  // Serve robots.txt with correct MIME type
+  app.get("/robots.txt", (_req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    const robotsPath = path.join(distPath, "robots.txt");
+    if (fs.existsSync(robotsPath)) {
+      res.sendFile(robotsPath);
+    } else {
+      res.status(404).send("Robots.txt not found");
+    }
+  });
 
   // Serve PWA manifest with correct MIME type
   app.get("/manifest.json", (_req, res) => {
@@ -21,7 +40,6 @@ export function serveStatic(app: Express) {
       res.status(404).send("Manifest not found");
     }
   });
-
   // Serve service worker with correct headers
   app.get("/service-worker.js", (_req, res) => {
     res.setHeader("Content-Type", "application/javascript");
@@ -34,7 +52,6 @@ export function serveStatic(app: Express) {
       res.status(404).send("Service worker not found");
     }
   });
-
   // Serve static files with proper caching
   app.use(
     express.static(distPath, {
@@ -50,7 +67,6 @@ export function serveStatic(app: Express) {
       },
     }),
   );
-
   // SPA fallback - serve index.html for all other routes
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
