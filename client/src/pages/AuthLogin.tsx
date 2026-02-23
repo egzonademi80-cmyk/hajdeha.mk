f9a4d43ae8672efa5cb27ef9978ebaae18f2f0c2
egzonademi80-cmyk <egzonademi80@gmail.com>
2026-02-23 23:56:11 +0000

    F

--- client/src/pages/AuthLogin.tsx
+++ client/src/pages/AuthLogin.tsx
@@ -237,6 +237,7 @@ export default function AuthLogin() {
       </div>
 
       {/* Right Panel - Login Form */}
+      {/* Right Panel - Login Form */}
       <div className="flex-1 flex flex-col">
         {/* Header */}
         <header className="flex items-center justify-between p-6 lg:p-8">
